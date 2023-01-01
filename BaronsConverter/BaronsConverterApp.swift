ce956ea509e55af3f88b752b546faf8a9fc42769
Paul Onawola <onawola.ire@gmail.com>
2023-01-01 23:05:46 +0100

    Initial Commit

---
+++ BaronsConverter/BaronsConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaronsConverterApp.swift
+//  BaronsConverter
+//
+//  Created by Paul Onawola on 01/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BaronsConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
